0b37790bea7b98f08a4a7592d816e1a16e31708c
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2023-11-27 17:37:24 -0300

    searching pokemons

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="column is-half is-offset-one-quarter">
-      <div v-for="(poke, index) in pokemons" :key="index">
+      <div class="is-flex-direction-row">
+        <input class="input is-rouded" type="text" placeholder="Buscar pelo nome" v-model="busca">
+        <button class="button is-fullwidth is-success" id="buscaBtn" @click="buscar">Buscar</button>
+      </div>
+
+      <div v-for="(poke, index) in filteredPokemons" :key="poke.url">
         <PokemonComponent :name="poke.name" :url="poke.url" :num="index+1" />
       </div>
     </div>
@@ -14,18 +19,40 @@ export default {
   name: 'App',
   data(){
     return {
-      pokemons: []
+      pokemons: [],
+      filteredPokemons: [],
+      busca: ''
     }
   },
   created: function(){
     axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res => {
       console.log("Lista recebida");
       this.pokemons = res.data.results;
+      this.filteredPokemons = res.data.results;
     })
   },
+  methods: {
+    buscar: function(){
+      this.filteredPokemons = this.pokemons;
+      if(this.busca == '' || this.busca == ' '){
+        this.filteredPokemons = this.pokemons;
+      } else {
+        this.filteredPokemons = this.pokemons.filter(pokemon => pokemon.name == this.busca)
+      }
+    }
+  },  
   components: {
     PokemonComponent
   }
+  // computed: {
+  //   resultadoBusca: function(){
+  //     if(this.busca == '' || this.busca == ' '){
+  //       return this.pokemons;
+  //     } else {
+  //       return this.pokemons.filter(pokemon => this.pokemon.name == this.busca);
+  //     }
+  //   }
+  // }
 }
 </script>
 
@@ -37,5 +64,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#buscaBtn{
+  margin-top: 2%;
 }
 </style>
